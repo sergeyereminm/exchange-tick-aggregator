d2d6631bc68dcc12d21afd6d41cdace330caafe3
RentreadySergeyEremin <sergey.eremin@rentready.com>
2026-08-13 19:01:19 +0300

    Add failing concurrency test for tick deduplicator.

--- tests/ExchangeTickAggregator.Tests/Deduplication/TickDeduplicatorTests.cs
+++ tests/ExchangeTickAggregator.Tests/Deduplication/TickDeduplicatorTests.cs
@@ -19,4 +19,31 @@ public class TickDeduplicatorTests
         Assert.True(deduplicator.TryAccept(tick));
         Assert.False(deduplicator.TryAccept(tick));
     }
+
+    [Fact]
+    public void TryAccept_accepts_each_unique_tick_once_under_concurrent_load()
+    {
+        var deduplicator = new TickDeduplicator(TimeSpan.FromSeconds(30));
+        const int threadCount = 8;
+        const int ticksPerThread = 500;
+        var acceptedCount = 0;
+
+        Parallel.For(0, threadCount, threadIndex =>
+        {
+            for (var i = 0; i < ticksPerThread; i++)
+            {
+                var tick = new Tick(
+                    $"SYM-{threadIndex}-{i}",
+                    100m + i,
+                    1m,
+                    DateTimeOffset.UnixEpoch.AddMilliseconds(i),
+                    $"source-{threadIndex}");
+
+                if (deduplicator.TryAccept(tick))
+                    Interlocked.Increment(ref acceptedCount);
+            }
+        });
+
+        Assert.Equal(threadCount * ticksPerThread, acceptedCount);
+    }
 }
